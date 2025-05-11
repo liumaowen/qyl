16c871ef08ce2ec262d1e98a0c04f758d972ec18
liumaowen <769298546@qq.com>
2025-05-11 19:05:07 +0800

    删除

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import TabsPage from '../views/TabsPage.vue'
-import ViewVideoPage from '../views/ViewVideoPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
