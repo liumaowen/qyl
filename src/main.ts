0150cbb2b3bc8b7842be61aa1b3648973911545a
liumaowen <769298546@qq.com>
2025-06-14 20:11:16 +0800

    调整暗模式样式导入顺序，启用 dark.always.css，注释其他暗模式样式

--- src/main.ts
+++ src/main.ts
@@ -28,9 +28,9 @@ register();
  * https://ionicframework.com/docs/theming/dark-mode
  */
 
-/* @import '@ionic/vue/css/palettes/dark.always.css'; */
-/* @import '@ionic/vue/css/palettes/dark.class.css'; */
-import '@ionic/vue/css/palettes/dark.system.css';
+import '@ionic/vue/css/palettes/dark.always.css';
+/* @import '@ionic/vue/css/palettes/dark.class.css';
+import '@ionic/vue/css/palettes/dark.system.css'; */
 
 /* Theme variables */
 import './theme/variables.css';
