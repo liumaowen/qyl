9e01bdba4049c7439a246876998714d5b2080e3f
liumaowen <769298546@qq.com>
2025-09-16 09:41:17 +0800

    5

--- android/app/src/main/java/com/qyl/quanyouliao/MainActivity.java
+++ android/app/src/main/java/com/qyl/quanyouliao/MainActivity.java
@@ -3,12 +3,20 @@ package com.qyl.quanyouliao;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 import com.startapp.sdk.adsbase.StartAppSDK;
+import com.qyl.quanyouliao.ads.StartioAdsPlugin;
+
+import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Register custom plugins
+        registerPlugin(StartioAdsPlugin.class);
+        
         // Initialize Start.io SDK with APP ID
         StartAppSDK.init(this, "208466650", false);
         // Enable test ads for debugging
