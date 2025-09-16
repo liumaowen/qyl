3dc6432ae2385448db15402c93345757a9dc26e0
liumaowen <769298546@qq.com>
2025-09-16 10:48:35 +0800

    3

--- android/app/src/main/java/com/qyl/quanyouliao/MainActivity.java
+++ android/app/src/main/java/com/qyl/quanyouliao/MainActivity.java
@@ -1,20 +1,27 @@
 package com.qyl.quanyouliao;
 
 import android.os.Bundle;
-import java.util.ArrayList;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import com.startapp.sdk.adsbase.StartAppSDK;
 import com.qyl.quanyouliao.ads.StartioAdsPlugin;
+
+import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        
+        // Register custom plugins
+        registerPlugin(StartioAdsPlugin.class);
         super.onCreate(savedInstanceState);
         
-        // Initialize plugins list
-        ArrayList<Class<? extends Plugin>> additionalPlugins = new ArrayList<>();
-        additionalPlugins.add(StartioAdsPlugin.class);
-        init(savedInstanceState, additionalPlugins);
+        // Initialize Start.io SDK with APP ID
+        StartAppSDK.init(this, "208466650", false);
+        // Enable test ads for debugging
+        StartAppSDK.setTestAdsEnabled(true);
+        // Set user consent (required for GDPR compliance)
+        StartAppSDK.setUserConsent(this, "pas", System.currentTimeMillis(), true);
     }
 }
